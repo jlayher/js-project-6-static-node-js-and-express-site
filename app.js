0a0937dbffce7a15f7f0596528110b93a737813f
jlayher <john.e.layher@gmail.com>
2020-10-18 18:57:56 -0400

    Setting Up Routes but likely wrong

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const express = require('express');
 const app = express();
 const { projects } = "/data.json";
 const path = require('path'); 
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use('/about', routes);
